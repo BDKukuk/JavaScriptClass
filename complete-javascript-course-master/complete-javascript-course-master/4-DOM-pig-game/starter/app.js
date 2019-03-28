621b3a931cdb32edc17242c0f4410dd9dd331326
Brad <bradley@proactiveapps.com>
2019-03-28 13:05:14 -0700

    Changes after 2 lessons

--- complete-javascript-course-master/complete-javascript-course-master/4-DOM-pig-game/starter/app.js
+++ complete-javascript-course-master/complete-javascript-course-master/4-DOM-pig-game/starter/app.js
@@ -8,3 +8,30 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+var scores, roundScore, activePlayer, dice;
+
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 1;
+
+
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function btn()
+{
+  // 1. Get random number
+
+  dice = Math.floor(Math.random() * 6) + 1;
+
+  //2.
+  var diceDOM = document.querySelector('.dice');
+  diceDOM.style.display = 'block';
+  diceDOM.src = 'dice-' + dice + '.png';
+});
